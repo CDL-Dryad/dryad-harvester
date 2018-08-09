04536b3de3281bef38f1096fc570250c172325d0
Scott Fisher <scott.fisher@ucop.edu>
2018-08-09 11:42:16 -0700

    Minor changes to get harvester working.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-# lock '3.4.0'
+# lock '3.4.1'
 
 set :application, 'dryad-harvester'
 set :repo_url, 'https://github.com/CDL-Dryad/dryad-harvester.git'
